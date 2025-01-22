3bfff2110f93818ebcf36f3583944fe921667813
Lebogang Sekaleli <lebogangsekaleli@gmail.com>
2025-01-22 22:14:13 +0200

    Product Images Component

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -5,7 +5,6 @@ import { LATEST_PRODUCTS_LIMIT } from '../constants';
 
 // Get the latest products
 export async function getLatestProducts() {
-  
   // Fetch the latest 4 products
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
@@ -13,4 +12,11 @@ export async function getLatestProducts() {
   });
 
   return convertToPlainObject(data);
+}
+
+// Get single product by it's slug
+export async function getProductBySlug(slug: string) {
+  return await prisma.product.findFirst({
+    where: { slug: slug },
+  });
 }
