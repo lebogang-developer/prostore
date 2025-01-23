a5a646c016cb3d51940785899a2fe8da69122909
Lebogang Sekaleli <lebogangsekaleli@gmail.com>
2025-01-23 08:48:24 +0200

    Seed user data

--- db/sample-data.ts
+++ db/sample-data.ts
@@ -1,4 +1,20 @@
+import { hashSync } from 'bcrypt-ts-edge';
+
 const sampleData = {
+  users: [
+    {
+      name: 'Peter',
+      email: 'admin@example.com',
+      password: hashSync('123456', 10),
+      role: 'admin',
+    },
+    {
+      name: 'Jane',
+      email: 'user@example.com',
+      password: hashSync('123456', 10),
+      role: 'admin',
+    },
+  ],
   products: [
     {
       name: 'Polo Sporting Stretch Shirt',
