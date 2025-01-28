53a76d6fb980c55b0719fe462164263a3557db81
Lebogang Sekaleli <lebogangsekaleli@gmail.com>
2025-01-28 22:43:28 +0200

    Sign In page

--- lib/validators.ts
+++ lib/validators.ts
@@ -21,3 +21,9 @@ export const insertProductsSchema = z.object({
   banner: z.string().nullable(),
   price: currency,
 });
+
+// Schema for signing In users
+export const signInFormSchema = z.object({
+  email: z.string().email('Invalid email address'),
+  password: z.string().min(6, 'Password must be at least characters')
+});
