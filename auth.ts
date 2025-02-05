0c875947ac87827be28ed6a8c4af9663d949500e
Lebogang Sekaleli <lebogangsekaleli@gmail.com>
2025-02-05 16:26:38 +0200

    Authentication with Next Auth complete

--- auth.ts
+++ auth.ts
@@ -1,4 +1,3 @@
-
 import NextAuth from 'next-auth';
 import { prisma } from '@/db/prisma';
 import { PrismaAdapter } from '@auth/prisma-adapter';
@@ -6,19 +5,18 @@ import type { NextAuthConfig } from 'next-auth';
 import { compareSync } from 'bcrypt-ts-edge';
 import CredentialsProvider from 'next-auth/providers/credentials';
 
-
-export const config() {
+export const config = {
   pages: {
-    signIn: '/sign-in';
-    error: '/sign-in';
-  };
+    signIn: '/sign-in',
+    error: '/sign-in',
+  },
   session: {
-    strategy: 'jwt';
-    maxAge: 30 * 24 * 60 * 60; // 30 days
-  };
+    strategy: 'jwt',
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
   adapter: PrismaAdapter(prisma),
   providers: [
-    CredentialsProvider ({
+    CredentialsProvider({
       credentials: {
         email: { type: 'email' },
         password: { type: 'password' },
@@ -31,43 +29,63 @@ export const config() {
           where: {
             email: credentials.email as string,
           },
-    });
-     // Check if user exists and if the password matches
-     if (user && user.password) {
-      const isMatch = await compareSync(
-        credentials.password as string,
-        user.password
-      );
+        });
+        // Check if user exists and if the password matches
+        if (user && user.password) {
+          const isMatch = await compareSync(
+            credentials.password as string,
+            user.password
+          );
 
-      // If password is correct, return user
-      if (isMatch) {
-        return {
-          id: user.id,
-          name: user.name,
-          email: user.email,
-          role: user.role,
-        };
-      }
-    }
-    // If user does not exist or password does not match return null
-    return null;
-  },
-}),
-  ];
-  callback: {
-    async session ({ session, user, trigger, token }: any ) {
+          // If password is correct, return user
+          if (isMatch) {
+            return {
+              id: user.id,
+              name: user.name,
+              email: user.email,
+              role: user.role,
+            };
+          }
+        }
+        // If user does not exist or password does not match return null
+        return null;
+      },
+    }),
+  ],
+
+  callbacks: {
+    async session({ session, user, trigger, token }: any) {
       // Set the user ID from the token
       session.user.id = token.sub;
-      
+      session.user.role = token.role;
+      session.user.name = token.name;
+
+      console.log(token);
+
       // If there is an update, set the user name
       if (trigger === 'update') {
         session.user.name = user.name;
       }
-
       return session;
+    },
+    async jwt({ token, user, trigger, session }: any) {
+      // Assign user fields to the token
+      if (user) {
+        token.role = user.role;
 
-    }
-  }
-}satisfies NextAuthConfig;
+        // If user has no name, then use the email
+        if (user.name === 'NO_NAME') {
+          token.name = user.email!.split('@')[0];
+          // Update database to reflect the token name
+          await prisma.user.update({
+            where: { id: user.id },
+            data: { name: token.name },
+          });
+        }
+      }
+      return token;
+    },
+  },
+} satisfies NextAuthConfig;
 
-export const {handlers, auth, signIn, signOut} = NextAuth(config);
+export const { handlers, auth, signIn, signOut } = NextAuth(config);
--- lib/validators.ts
+++ lib/validators.ts
@@ -22,20 +22,20 @@ export const insertProductsSchema = z.object({
   price: currency,
 });
 
-// Schema for signing in a user
+// Schema for signing in users
 export const signInFormSchema = z.object({
   email: z.string().email('Invalid email address'),
-  password: z.string().min(6, 'Password must be at least characters'),
+  password: z.string().min(6, 'Password must be at least 6 characters'),
 });
 // Schema for signing up a user
 export const signUpFormSchema = z
   .object({
     name: z.string().min(3, 'Name must be at least 3 characters'),
     email: z.string().email('Invalid email address'),
-    password: z.string().min(6, 'Password must be at least characters'),
+    password: z.string().min(6, 'Password must be at least 6 characters'),
     confirmPassword: z
       .string()
-      .min(6, 'Confirm password must be at least characters'),
+      .min(6, 'Confirm password must be at least 6 characters'),
   })
   .refine((data) => data.password === data.confirmPassword, {
     message: 'Passwords do not match!',
